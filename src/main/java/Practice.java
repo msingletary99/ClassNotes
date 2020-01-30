8838d9d06c0f9f550da14c2da039aa1794b286a6
0781317 <0781317@KELCE-LABN10-P.pittstate.edu>
2020-01-30 09:04:03 -0600

    Names From 1/30/2020

---
+++ src/main/java/Practice.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0781317
+ */
+public class Practice {
+    public static void main(String[] args){
+    
+    System.out.println("Class 1:\t Intermediate Programming");
+    System.out.println("Class 2:\t System Analysis and Design");
+    
+    System.out.println("Hello World!");
+    System.out.println("Hello Again World!");
+    
+    System.out.print("This will all");
+    System.out.print(" be in one line!");
+    
+    System.out.print("\nThis will NOT \n");
+    System.out.print("be in one line!");
+    
+    String outputText=" Part 2!";
+    
+    System.out.println("\nHello Again World"+outputText);
+    
+/* cannot start a variable with an interger*/  
+/* Variable names should be descriptive*/
+    double salesTaxRate=0.0725;
+    
+/* Java programs should be self documenting*/
+
+/* Naming Conventions
+        Variables: Should start with a lowercase and alternate
+        Class: Should be all Uppercase
+/*
+
+/*Types of data
+Byte: -128 to 127
+Shorts: -32768 to 32767
+int: -2.1 billion to 2.1 billion
+long: 9 centillion with decimals   
+Float: single precision up to 7 decimal points
+double: Double precision up to 15 decimal points
+*/
+    int inches=12;
+    float temperatureInF=15.5F;
+    byte myNumber;
+        myNumber=2;
+//If you want to float you have to put an F after the number to recogonize it
+
+//You cannot put a sign or commas in your literal you will have to format during print
+
+    boolean itsWarmOut=false;
+    
+    char firstLetterOfMyFirstName='M';
+    String firstLetterOfMyLastName="S";
+// char in single quotes, String in double quotes
+
+    int month, days;
+    month=1;
+    days=31;
+    System.out.println("Month "+month+" has "+days+" days!");
+    
+    
+    
+    
+    
+    
+    
+     
+                
+    
+    
+ 
+    }   
+}
